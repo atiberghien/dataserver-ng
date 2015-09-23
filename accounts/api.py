d174d610a06ab69e2fb4ae6e786a029cef826b37
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-09-23 16:51:42 +0200

    Implements abuse reporting

--- accounts/api.py
+++ accounts/api.py
@@ -88,7 +88,29 @@ class UserResource(ModelResource):
             url(r'^(?P<resource_name>%s)/(?P<user_id>\d+)/send/message%s$' %
                 (self._meta.resource_name, trailing_slash()),
                 self.wrap_view('send_message'), name='api_send_message'),
+            url(r'^(?P<resource_name>%s)/report/abuse%s$' %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('report_abuse'), name='api_report_abuse'),
         ]
+
+    def send_email(self, request, data, recipients):
+        payload = dict(secret=settings.GOOGLE_RECAPTCHA_SECRET,
+                       response=data['recaptcha_response'])
+
+        r = requests.post("https://www.google.com/recaptcha/api/siteverify", data=payload)
+        resp = json.loads(r.text)
+        if resp["success"] and len(recipients):
+            try:
+                send_mail(data["subject"],
+                        data["body"],
+                        data["sender_email"],
+                        recipients,
+                        fail_silently=False)
+            except:
+                return self.create_response(request, {'success': False})
+            return self.create_response(request, {'success': True})
+        else:
+            return self.create_response(request, {'success': False})
 
     def send_message(self, request, **kwargs):
         """
@@ -98,30 +120,22 @@ class UserResource(ModelResource):
         data = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
 
         try:
-            recipient = User.objects.get(id=kwargs["user_id"])
+            recipients = [User.objects.get(id=kwargs["user_id"]).values_list('email', flat=True)]
         except User.DoesNotExist:
             return self.create_response(request, {'success': False})
 
+        return self.send_email(request, data, recipients)
 
-        payload = dict(secret=settings.GOOGLE_RECAPTCHA_SECRET,
-                       response=data['recaptcha_response'])
+    def report_abuse(self, request, **kwargs):
+        """
+        Send a message to user after checking captcha with google service
+        """
+        self.method_check(request, allowed=['post'])
+        data = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
 
-        r = requests.post("https://www.google.com/recaptcha/api/siteverify", data=payload)
-        resp = json.loads(r.text)
-        if resp["success"] :
-            try:
-                send_mail('Message Makerscience de %s' % data["sender_full_name"],
-                        data["body"],
-                        data["sender_email"],
-                        [recipient.email],
-                        fail_silently=False)
-            except:
-                return self.create_response(request, {'success': False})
-            return self.create_response(request, {'success': True})
-        else:
-            return self.create_response(request, {'success': False})
+        recipients = User.objects.filter(is_superuser=True).values_list('email', flat=True)
 
-
+        return self.send_email(request, data, recipients)
 
     def login_google(self, request, **kwargs):
         """
